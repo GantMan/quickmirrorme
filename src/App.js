ab4804af4ae4db128c4d7f45feebd4368887e274
Gant Laborde <gantman@gmail.com>
2020-10-28 16:30:07 -0500

    basics working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useRef} from 'react'
 
 function App() {
+  const mirror = useRef(null)
+
+  async function setupVideo() {
+    const maxWidth = window.innerWidth;
+    const maxHeight = window.innerHeight;
+
+    const videoConstraints = {
+      width: { ideal: maxWidth, max: maxWidth},
+      height: { ideal: maxHeight, max: maxHeight},
+      facingMode: 'environment' // rear facing if possible
+    }
+
+    const video = mirror.current
+    video.width = maxWidth
+    video.height = maxHeight
+
+    try {
+      const vidStream = await navigator.mediaDevices.getUserMedia({audio: false, video: videoConstraints})
+
+      const videoInputs = vidStream.getVideoTracks()
+      console.log(`YO the video device I am seeing is ${videoInputs[0].label}`)
+
+      if ('srcObject' in video) {
+        video.srcObject = vidStream
+      } else {
+        video.src = window.URL.createObjectURL(vidStream)
+      }
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    setupVideo()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          QuickMirror
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <video ref={mirror} id="mirror" autoPlay />
+      </main>
     </div>
   );
 }
