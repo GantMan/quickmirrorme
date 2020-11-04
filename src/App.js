0fc2cfbbe518b1f6383fea66cabc051d0d2c1970
Gant Laborde <gantman@gmail.com>
2020-11-04 12:01:42 -0600

    fix scrolling

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   async function setupVideo(useDevice) {
     console.log('Setting up with ', currentDevice)
     listMediaDevices()
-    const maxWidth = window.innerWidth;
+    const maxWidth = window.innerWidth - 18; // subtract scrollbar
     const maxHeight = window.innerHeight;
     const deviceId = useDevice ? { exact: useDevice } : null
 
